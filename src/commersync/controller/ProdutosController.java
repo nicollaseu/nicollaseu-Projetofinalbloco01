c9c7fa9df873051b344f6c3fc1f8f5c1fc52eb14
Nicollas <nsilvademoraes@gmail.com>
2024-06-20 15:53:53 -0300

    ProdutosControlerv1

--- src/commersync/controller/ProdutosController.java
+++ src/commersync/controller/ProdutosController.java
@@ -1,4 +1,36 @@
 package commersync.controller;
 
-public class ProdutosController {
+import commersync.repository.Interface;
+
+public class ProdutosController implements Interface {
+
+    @Override
+    public void produtos(int produtos) {
+
+    }
+
+    @Override
+    public void pedidoDeCompra(int pedidoDeCompra) {
+
+    }
+
+    @Override
+    public void listaProdutosTelefone(int listaProdutosTelefone) {
+
+    }
+
+    @Override
+    public void cancelarContaPerfil(int cancelarContaPerfil) {
+
+    }
+
+    @Override
+    public void marcaCelular(int marcaCelular) {
+
+    }
+
+    @Override
+    public void cancelaCompra(int cancelarCompra) {
+
+    }
 }
