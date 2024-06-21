4601f421bdeb701b42a12214c5dc1afcae16d916
Nicollas <nsilvademoraes@gmail.com>
2024-06-21 19:55:16 -0300

    Finalizando Projeto

--- src/commersync/controller/ProdutosController.java
+++ src/commersync/controller/ProdutosController.java
@@ -7,31 +7,31 @@ public class ProdutosController implements Interface {
 
     @Override
     public void produtos(int produtos) {
-        
+        System.out.println("Produto adicionado com ID: " + produtos);
     }
 
     @Override
     public void pedidoDeCompra(int pedidoDeCompra) {
-
+        System.out.println("Pedido de compra realizado com ID: " + pedidoDeCompra);
     }
 
     @Override
     public void listaProdutosTelefone(int listaProdutosTelefone) {
-
+        System.out.println("Listando produtos para telefone com ID: " + listaProdutosTelefone);
     }
 
     @Override
     public void cancelarContaPerfil(int cancelarContaPerfil) {
-
+        System.out.println("Conta perfil cancelada com ID: " + cancelarContaPerfil);
     }
 
     @Override
     public void marcaCelular(int marcaCelular) {
-
+        System.out.println("Marca de celular selecionada com ID: " + marcaCelular);
     }
 
     @Override
     public void cancelaCompra(int cancelarCompra) {
-
+        System.out.println("Compra cancelada com ID: " + cancelarCompra);
     }
 }
--- src/commersync/util/Menu.java
+++ src/commersync/util/Menu.java
@@ -1,4 +1,6 @@
 package commersync.util;
+import commersync.controller.ProdutosController;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -19,6 +21,7 @@ public class Menu {
     }
 
     public static void main(String[] args) {
+        ProdutosController produtosController = new ProdutosController();
         Scanner leia = new Scanner(System.in);
         int opcao;
 
@@ -32,7 +35,7 @@ public class Menu {
                 opcao = 0;
             }
 
-            if (opcao == 7) {
+            if (opcao == 6) {
                 //System.out.println(Cores.TEXT_WHITE_BOLD + "\n!");
                 System.out.println("Saindo do sistema. Até mais!");
                 leia.close();
@@ -42,18 +45,33 @@ public class Menu {
             switch (opcao) {
                 case 1:
                     System.out.println("Escolha Seu Produto");
+                    System.out.print("Digite o ID do produto: ");
+                    int produtoId = leia.nextInt();
+                    produtosController.produtos(produtoId);
                     break;
                 case 2:
                     System.out.println("Realize seu Pedido de Compra");
+                    System.out.print("Digite o ID do pedido: ");
+                    int pedidoId = leia.nextInt();
+                    produtosController.pedidoDeCompra(pedidoId);
                     break;
                 case 3:
                     System.out.println("Lista de Telefones");
+                    System.out.print("Digite o ID do telefone: ");
+                    int telefoneId = leia.nextInt();
+                    produtosController.listaProdutosTelefone(telefoneId);
                     break;
                 case 4:
                     System.out.println("Cancelar Conta");
+                    System.out.print("Digite o ID do perfil: ");
+                    int perfilId = leia.nextInt();
+                    produtosController.cancelarContaPerfil(perfilId);
                     break;
                 case 5:
                     System.out.println("Cancelar Seu Pedido de Compra");
+                    System.out.print("Digite o ID da compra: ");
+                    int compraId = leia.nextInt();
+                    produtosController.cancelaCompra(compraId);
             }
         }
     }
